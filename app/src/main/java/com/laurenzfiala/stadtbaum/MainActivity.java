00daa2fe3d552d6a6f529bf1b5d746caae580319
laurenzfiala <laurenz.fiala@outlook.at>
2017-09-18 21:13:22 +0200

    initial commit

---
+++ app/src/main/java/com/laurenzfiala/stadtbaum/MainActivity.java
@@ -0,0 +1,30 @@
+package com.laurenzfiala.stadtbaum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.panel_display);
+
+        this.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error); // TODO show error
+            }
+        });
+
+        this.webView.loadUrl("http://google.at/");
+    }
+}
